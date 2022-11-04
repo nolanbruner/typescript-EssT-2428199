26ba226b669a9e433fc3290bf59b853c4ac56635
nolanbruner <nolansname@aol.com>
2022-11-04 12:43:51 -0400

    challange completed

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,28 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+interface todoType {
+    id: number,
+    title: string,
+    status: string,
+    completedOn?: Date
+
+}
+enum Status {
+    todo = "todo",
+    done = "done",
+    inProgress = "in-progress"
+}
+const todoItems: todoType[] = [
+    { id: 1, title: "Learn HTML", status: Status.done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: Status.inProgress },
+    { id: 3, title: "Write the best app in the world", status: Status.todo },
 ]
 
-function addTodoItem(todo) {
+function addTodoItem(todo:string):todoType {
     const id = getNextId(todoItems)
 
     const newTodo = {
         id,
         title: todo,
-        status: "todo",
+        status: Status.todo,
     }
 
     todoItems.push(newTodo)
@@ -18,7 +30,7 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+function getNextId<T extends{id:number}>(items:T[]):number {
     return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
 }
 
